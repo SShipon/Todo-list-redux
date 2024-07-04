e818c4c5b364c812f9651e3aab6cccd02150d6ff
SShipon <nxshipon@gmail.com>
2024-07-04 12:12:13 +0100

    text responisve

--- src/pages/Todo.tsx
+++ src/pages/Todo.tsx
@@ -5,11 +5,11 @@ import Container from "@/components/ui/Container";
 const Todo = () => {
     return (
         <Container>
-<h2 className="text-center text-sm md:text-2xl lg:text-3xl font-semibold my-10 md:max-w-screen-md md:mx-auto">
+<h2 className="text-center text-base md:text-2xl lg:text-3xl font-semibold my-5 md:max-w-screen-md md:mx-auto">
     <span className="bg-clip-text text-transparent bg-gradient-to-r from-purple-500 to-blue-500 block md:inline-block">
         Write the history of your life,
     </span><br className="md:hidden" />
-    <span className="bg-clip-text text-transparent bg-gradient-to-r from-green-500 to-yellow-500 block md:inline-block">
+    <span className="bg-clip-text text-base text-transparent bg-gradient-to-r from-green-500 to-yellow-500 block md:inline-block">
         your good sides and bad sides
     </span>
 </h2>
